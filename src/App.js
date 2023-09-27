c76197de11c7d9edfccce075879c7aac4c6eb342
parvathysanthosh21 <parvathysanthosh231@gmail.com>
2023-09-27 15:24:10 +0530

    vedio-app-new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import Home from './Pages/Home';
+import Landingpage from './Pages/Landingpage';
 
 function App() {
   return (
     <div className="App">
       <h1>Vedio App</h1>
-
+<Home/>
+<Landingpage/>
     </div>
   );
-}
+  }
 
 export default App;
